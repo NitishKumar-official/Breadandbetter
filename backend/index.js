09559813bbdfd34d35d1f03ff1823569dd2ae2fa
nitish <kumarnitish27212@gmail.com>
2024-12-19 19:12:07 +0530

    dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config();
+  }
+
 import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
